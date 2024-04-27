db308efc8a025aac9943a0054a49373d980a0634
Jacob Desilets <jacobdesilets@gmail.com>
2024-04-27 19:13:21 -0400

    Initial commit

---
+++ src/main/java/net/jacob/jacobmod/JacobModClient.java
@@ -0,0 +1,10 @@
+package net.jacob.jacobmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JacobModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
